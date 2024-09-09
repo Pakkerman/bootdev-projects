559a7098e530acc3cf48b3801781e736b0c1e448
Pakkerman <104409243+Pakkerman@users.noreply.github.com>
2024-09-09 09:35:54 +0800

    05_main

--- main.go
+++ main.go
@@ -2,28 +2,30 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
 func main() {
-	// if len(os.Args) < 2 {
-	// 	fmt.Println("no website provided")
-	// 	os.Exit(1)
-	//
-	// } else if 2 < len(os.Args) {
-	// 	fmt.Println("too many arguments provided")
-	// 	os.Exit(1)
-	// }
-	//
-	// url := os.Args[1]
-	//
-	// fmt.Println("starting crawl of:", url)
+	if len(os.Args) < 2 {
+		fmt.Println("no website provided")
+		os.Exit(1)
 
-	url := "https://blog.boot.dev"
+	} else if 2 < len(os.Args) {
+		fmt.Println("too many arguments provided")
+		os.Exit(1)
+	}
+
+	url := os.Args[1]
+
+	fmt.Println("starting crawl of:", url)
+
 	body, err := getBody(url)
 	if err != nil {
 		fmt.Println(err)
 	}
 
 	urls, _ := getURLsFromHTML(body, url)
-	fmt.Println(urls)
+	for _, url := range urls {
+		fmt.Println(url)
+	}
 }
