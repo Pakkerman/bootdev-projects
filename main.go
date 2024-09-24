17113eece192021a1474ac403e8857aa681cdc13
Pakkerman <104409243+Pakkerman@users.noreply.github.com>
2024-09-24 14:54:58 +0800

    09_max pages

--- main.go
+++ main.go
@@ -4,28 +4,28 @@ import (
 	"fmt"
 	"net/url"
 	"os"
+	"strconv"
 	"sync"
 	"time"
 )
 
 type config struct {
-	pages   map[string]int
-	baseURL *url.URL
-	mu      *sync.Mutex
-	wg      *sync.WaitGroup
-	buffer  chan struct{}
-	counter int
+	pages          map[string]int
+	baseURL        *url.URL
+	mu             *sync.Mutex
+	wg             *sync.WaitGroup
+	buffer         chan struct{}
+	counter        int
+	maxPages       int
+	maxConcurrency int
 }
 
 func main() {
 	start := time.Now()
 
-	if len(os.Args) < 2 {
-		fmt.Println("no website provided")
-		os.Exit(1)
-
-	} else if 2 < len(os.Args) {
-		fmt.Println("too many arguments provided")
+	fmt.Println(len(os.Args))
+	if len(os.Args) < 2 || 5 <= len(os.Args) {
+		fmt.Println("# usage: ./crawler URL maxConcurrency maxPages")
 		os.Exit(1)
 	}
 
@@ -35,29 +35,55 @@ func main() {
 		os.Exit(1)
 	}
 
-	fmt.Println("starting crawl of:", parsedURL.String())
+	maxConcurrency := 5
+	if 2 < len(os.Args) && os.Args[2] != "" {
+		val, err := strconv.Atoi(os.Args[2])
+		if err != nil {
+			fmt.Println("# usage: ./crawler URL maxConcurrency maxPages")
+			return
+		}
+		maxConcurrency = val
+	}
+
+	maxPages := 10
+	if 3 < len(os.Args) && os.Args[3] != "" {
+		val, err := strconv.Atoi(os.Args[3])
+		if err != nil {
+			fmt.Println("# usage: ./crawler URL maxConcurrency maxPages")
+			return
+		}
+		maxPages = val
+	}
 
 	cfg := config{
-		pages:   make(map[string]int),
-		baseURL: parsedURL,
-		mu:      &sync.Mutex{},
-		wg:      &sync.WaitGroup{},
-		buffer:  make(chan struct{}, 10),
-		counter: 0,
+		pages:          make(map[string]int),
+		baseURL:        parsedURL,
+		mu:             &sync.Mutex{},
+		wg:             &sync.WaitGroup{},
+		buffer:         make(chan struct{}, 10),
+		counter:        0,
+		maxConcurrency: maxConcurrency,
+		maxPages:       maxPages,
 	}
+
+	fmt.Println("Concurrent requests: ", maxConcurrency)
+	fmt.Println("Max Pages to crawl: ", maxPages)
 
 	cfg.crawlPage(cfg.baseURL.String())
 	cfg.wg.Wait()
 
+	fmt.Println("=============================")
+	fmt.Println("REPORT for ", cfg.baseURL)
+	fmt.Println("=============================")
 	fmt.Printf("Pages crawled: %v\n", cfg.counter)
 	fmt.Printf("Elapsed time: %s\n", time.Since(start))
+	cfg.printReport()
 }
 
 func (cfg *config) crawlPage(rawCurrentURL string) {
 	cfg.wg.Add(1)
 	defer func() {
 		cfg.wg.Done()
-		cfg.incrementCounter()
 	}()
 
 	fmt.Println("crawling page:", rawCurrentURL)
@@ -93,18 +119,20 @@ func (cfg *config) crawlPage(rawCurrentURL string) {
 		unvisited = append(unvisited, link)
 	}
 
-	if len(unvisited) == 0 {
-		return
-	}
-
-	fmt.Println("\tunvisited urls on this page: ", len(unvisited))
+	// fmt.Println("\tunvisited urls on this page: ", len(unvisited))
 
 	for _, link := range unvisited {
+		cfg.incrementCounter()
+		fmt.Println(cfg.counter)
 		go func(link string) {
 			cfg.buffer <- struct{}{}
 			cfg.crawlPage(link)
 			<-cfg.buffer
 		}(link)
+
+		if cfg.pagesMaxed() {
+			break
+		}
 	}
 }
 
@@ -127,4 +155,17 @@ func (cfg *config) incrementCounter() {
 	defer cfg.mu.Unlock()
 
 	cfg.counter++
+}
+
+func (cfg config) pagesMaxed() bool {
+	cfg.mu.Lock()
+	defer cfg.mu.Unlock()
+
+	return cfg.maxPages <= cfg.counter
+}
+
+func (cfg config) printReport() {
+	for key, value := range cfg.pages {
+		fmt.Println(key, value)
+	}
 }
